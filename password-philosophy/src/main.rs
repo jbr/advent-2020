d7171dfc2c35c130cff647ce12ad8bf5cfde62f4
Jacob Rothstein <hi@jbr.me>
2020-12-01 22:26:27 -0800

    password-philosophy part 2
    this was less fun

--- password-philosophy/src/main.rs
+++ password-philosophy/src/main.rs
@@ -5,12 +5,14 @@ const INPUT: &str = include_str!("../input.txt");
 fn main() {
     let parser = Regex::new(r"(\d+)-(\d+) (.): (.+)\n?").unwrap();
     let valid_rows = parser.captures_iter(INPUT).filter(|row| {
-        let min = row[1].parse::<usize>().unwrap();
-        let max = row[2].parse::<usize>().unwrap();
+        let first_index = row[1].parse::<usize>().unwrap() - 1;
+        let second_index = row[2].parse::<usize>().unwrap() - 1;
         let pat: &str = &row[3];
         let pass: &str = &row[4];
-        let count = pass.matches(pat).count();
-        (min..=max).contains(&count)
+
+        let first_match = &pass[first_index..first_index + pat.len()] == pat;
+        let second_match = &pass[second_index..second_index + pat.len()] == pat;
+        first_match ^ second_match
     });
 
     println!("valid rows: {}", valid_rows.count());
