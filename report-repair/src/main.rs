0eefb991e4627c66cb70fcd2813f277d8ba7e163
Jacob Rothstein <hi@jbr.me>
2020-12-01 17:11:58 -0800

    second part

--- report-repair/src/main.rs
+++ report-repair/src/main.rs
@@ -1,27 +1,27 @@
-use std::collections::HashSet;
-
-fn find_pair_that_sums_to(input: &str, target_sum: usize) -> (usize, usize) {
-    let mut seen = HashSet::new();
-
-    for line in input.lines() {
-        let entry: usize = line.parse().unwrap();
-        if entry < target_sum {
-            let compliment = target_sum - entry;
-
-            if seen.contains(&entry) {
-                return (entry, compliment);
-            }
-
-            seen.insert(compliment);
-        }
-    }
-
-    panic!("unable to find two numbers that sum to {}", target_sum);
-}
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../input.txt");
+const TARGET: usize = 2020;
 
 fn main() {
-    let (a, b) = find_pair_that_sums_to(&INPUT, 2020);
-    println!("{} * {} = {}", a, b, a * b);
+    let mut compliments_and_products: HashMap<usize, usize> = HashMap::new();
+    let mut seen_numbers: Vec<usize> = Vec::with_capacity(200);
+
+    for line in INPUT.lines() {
+        let number = line.parse().unwrap();
+
+        if let Some(product) = compliments_and_products.get(&number) {
+            println!("{}", product * number);
+            return;
+        }
+
+        for other_number in &seen_numbers {
+            let sum = number + other_number;
+            if sum < TARGET {
+                compliments_and_products.insert(TARGET - sum, number * other_number);
+            }
+        }
+
+        seen_numbers.push(number);
+    }
 }
