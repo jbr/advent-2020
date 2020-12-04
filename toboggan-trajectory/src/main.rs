148abc5c904bb47750e3555b58c924eac0694d08
Jacob Rothstein <hi@jbr.me>
2020-12-03 19:46:10 -0800

    toboggan part ii

--- toboggan-trajectory/src/main.rs
+++ toboggan-trajectory/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
-    println!(
-        "trees: {}",
-        include_bytes!("../input.txt")
-            .split(|c| c == &b'\n')
-            .filter(|line| line.len() > 0)
-            .enumerate()
-            .filter(|(line_number, line)| line[(line_number * 3) % line.len()] == b'#')
-            .count()
-    );
+    let input = include_bytes!("../input.txt");
+    let product_of_trees: usize = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|(x, y)| dbg!(check_slope(input, *x, *y)))
+        .product();
+
+    println!("product of encountered trees: {}", product_of_trees);
+}
+
+fn check_slope(input: &[u8], x: usize, y: usize) -> usize {
+    input
+        .split(|c| c == &b'\n')
+        .filter(|line| line.len() > 0)
+        .step_by(y)
+        .enumerate()
+        .filter(|(line_number, line)| line[(*line_number * x) % line.len()] == b'#')
+        .count()
 }
